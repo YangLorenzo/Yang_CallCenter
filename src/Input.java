b01a8090225d9d7b1903090b02e2759f674709c2
yangrongwei <yangrongwei2002@gmail.com>
2022-04-25 12:05:32 +0200

    legge anche il codice dall'utente

--- src/Input.java
+++ src/Input.java
@@ -11,7 +11,7 @@ public class Input {
             try {
                 out.print("cosa vuoi fare? ");
                 op = in.nextInt();
-                if ((op < 1 || op > 6) && op != 0) throw new IllegalArgumentException();
+                if ((op < 1 || op > 8) && op != 0) throw new IllegalArgumentException();
                 return op;
             } catch (IllegalArgumentException e) {
                 out.println("numero invalido");
@@ -54,10 +54,12 @@ public class Input {
     }
 
     public static int getCodiceOperatore() {
+        out.print("inserisci il codice dell'operatore: ");
         return in.nextInt();
     }
 
     public static String getCodiceFiscale() {
+        out.print("inserisci il codice fiscale: ");
         return in.next();
     }
 
